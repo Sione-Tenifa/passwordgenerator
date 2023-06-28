cfb0649eed2bacc1ce01a1d21da72a9e2be69741
sione-tenifa <johntenifa@yahoo.com>
2023-06-28 17:52:42 -0600

    fixed notificatoin

--- src/App.js
+++ src/App.js
@@ -1,8 +1,49 @@
 import './App.css';
 import { FiCopy } from 'react-icons/fi';
+import { useState } from 'react';
+import { success } from './message';
+import {numbers, upperCaseLetters, lowerCaseLetters, specialCharacters} from './characters'
+import "react-toastify/dist/ReactToastify.css"
+import { toast, ToastContainer } from 'react-toastify';
 
-
+ 
 function App() {
+
+  const [password, setPassword] = useState("")
+  const [passwordLength, setPasswordLength] = useState('20')
+  const [includeUppercase, setIncludeUppercase] = useState(false)
+  const [includeLowercase, setIncludeLowercase] = useState(false)
+  const [includenumbers, setIncludeNumbers] = useState(false)
+  const [includeSymbols, setIncludeSymbols] = useState(false)
+
+  const notify = (message, hasError = false) => {
+    console.log('notify')
+    if(hasError){
+      console.log(message)
+      toast.error(message, {
+        position: 'top-center',
+        autoClose: 5000, 
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+      }
+
+      )
+    }
+  }
+
+  const handleGeneratePassword = (e) => {
+    console.log('hereeee')
+    if(!includeUppercase && !includeLowercase && !includeSymbols && !includenumbers){
+      console.log('in button')
+      notify('You Must Select at least one option', true)
+    }
+  }
+
+
+
   return (
     <div className="App">
       <div className='container'>
@@ -18,7 +59,8 @@ function App() {
           <div className='form-group'>
             <label htmlFor='password-strength'>Password Length </label>
             <input
-            defaultValue=''
+            defaultValue={passwordLength}
+            onChange={(e) => setPasswordLength(e.target.value)}
             type='number'
             id='password-strenght'
             name='password-strength'
@@ -31,6 +73,8 @@ function App() {
               <label htmlFor='uppercase-letters'>Include Uppercase</label>
               <input
               type='checkbox'
+              checked={includeUppercase}
+              onChange={(e) => setIncludeUppercase(e.target.checked)}
               id='uppercase-letters'
               name='uppercase-letters'
               />
@@ -40,6 +84,8 @@ function App() {
               <label htmlFor='lowercase-letters'>Include lowercase</label>
               <input
               type='checkbox'
+              checked={includeLowercase}
+              onChange={(e) => setIncludeLowercase(e.target.checked)}
               id='lowercase-letters'
               name='lowercase-letters'
               />
@@ -50,6 +96,8 @@ function App() {
               <input
               type='checkbox'
               id='include-numbers'
+              checked={includenumbers}
+              onChange={(e) => setIncludeNumbers(e.target.checked)}
               name='include-numbers'
               />
           </div>
@@ -59,11 +107,26 @@ function App() {
               <input
               type='checkbox'
               id='include-symbols'
+              checked={includeSymbols}
+              onChange={(e) => setIncludeSymbols(e.target.checked)}
               name='include-symbols'
               />
           </div>
 
-          <button className='generator__btn'>Generate Password</button>
+          <button onClick={handleGeneratePassword} className='generator__btn'>Generate Password</button>
+
+          <ToastContainer
+          position='top-center'
+          autoClose={5000}
+          hideProgressBar={false}
+          newestOnTop={false}
+          closeOnClick
+          rtl={false}
+          pauseOnFocusLoss
+          draggable
+          pauseOnHover
+          />
+
 
         </div>
       </div>
